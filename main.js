22fda01878d97a7f12fd0ee5f090f6df782e915b
lenyn29 <lenyn.alfonzo@gmail.com>
2021-02-03 14:17:16 -0400

    memory

---
+++ main.js
@@ -0,0 +1,58 @@
+const cards = document.querySelectorAll('.memory-card')
+
+let hasFlippedCard = false;
+let lookBoard = false;
+let firstCard, secondCard;
+
+function flipCard () {
+    if (lookBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+        hasFlippedCard = false;
+        secondCard = this;
+
+        checkForMatch();
+    }
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.color === secondCard.dataset.color
+
+    isMatch ? disableCard() : unflipCard();
+
+}
+
+function disableCard() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCard() {
+    lookBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    },1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lookBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
